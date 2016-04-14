fb70fa50ab419db562fc41a1e467e20aa197cc66
eric-greencomb <1121453646@qq.com>
2016-04-14 23:27:36 +0800

    add gather command

--- etcd_key_const.go
+++ etcd_key_const.go
@@ -5,9 +5,21 @@ const (
 	ETCD_KEY_GEARMAN_ADDR = "/banerwai/gearman/conn"
 )
 
+// Gearman
+const (
+	ETCD_KEY_REDIS_ADDR = "/banerwai/redis/conn"
+)
+
 // micros/query
 const (
 	ETCD_KEY_MICROS_QUERY_CATEGORY = "/banerwai/micros/query/category/addr"
 	ETCD_KEY_MICROS_QUERY_PROFILE  = "/banerwai/micros/query/profile/addr"
 	ETCD_KEY_MICROS_QUERY_RENDER   = "/banerwai/micros/query/render/addr"
+)
+
+// command/query
+const (
+	ETCD_KEY_MICROS_COMMAND_TOKEN = "/banerwai/micros/command/token/addr"
+	ETCD_KEY_MICROS_COMMAND_AUTH  = "/banerwai/micros/command/auth/addr"
+	// ETCD_KEY_MICROS_COMMAND_RENDER  = "/banerwai/micros/query/render/addr"
 )
