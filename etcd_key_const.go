ab5d356d0aee3b0c7934802b83e664e6ebabfbaf
eric-greencomb <1121453646@qq.com>
2016-04-24 22:26:39 +0800

    a .

--- etcd_key_const.go
+++ etcd_key_const.go
@@ -15,12 +15,14 @@ const (
 	ETCD_KEY_MICROS_QUERY_CATEGORY = "banerwai/micros/query/category/addr"
 	ETCD_KEY_MICROS_QUERY_PROFILE  = "banerwai/micros/query/profile/addr"
 	ETCD_KEY_MICROS_QUERY_RENDER   = "banerwai/micros/query/render/addr"
+
+	ETCD_KEY_MICROS_QUERY_AUTH = "banerwai/micros/query/auth/addr"
+	ETCD_KEY_MICROS_QUERY_USER = "banerwai/micros/query/user/addr"
 )
 
 // command/query
 const (
 	ETCD_KEY_MICROS_COMMAND_TOKEN   = "banerwai/micros/command/token/addr"
-	ETCD_KEY_MICROS_COMMAND_AUTH    = "banerwai/micros/command/auth/addr"
 	ETCD_KEY_MICROS_COMMAND_PROFILE = "banerwai/micros/command/profile/addr"
 	ETCD_KEY_MICROS_COMMAND_USER    = "banerwai/micros/command/user/addr"
 )
