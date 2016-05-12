453ed01e258afd0cf3b3fa268613215161b8e0f8
eric-greencomb <1121453646@qq.com>
2016-05-12 23:48:53 +0800

    add resume micro

--- etcd_key_const.go
+++ etcd_key_const.go
@@ -12,18 +12,30 @@ const (
 
 // micros/query
 const (
-	ETCD_KEY_MICROS_QUERY_CATEGORY = "banerwai/micros/query/category/addr"
-	ETCD_KEY_MICROS_QUERY_PROFILE  = "banerwai/micros/query/profile/addr"
-	ETCD_KEY_MICROS_QUERY_RENDER   = "banerwai/micros/query/render/addr"
+	ETCD_KEY_MICROS_QUERY_CATEGORY = "banerwai/micros/query/category/addr" // port: 39010
+	ETCD_KEY_MICROS_QUERY_RENDER   = "banerwai/micros/query/render/addr"   // port: 39030
 
-	ETCD_KEY_MICROS_QUERY_AUTH  = "banerwai/micros/query/auth/addr"
-	ETCD_KEY_MICROS_QUERY_USER  = "banerwai/micros/query/user/addr"
-	ETCD_KEY_MICROS_QUERY_TOKEN = "banerwai/micros/query/token/addr"
+	ETCD_KEY_MICROS_QUERY_TOKEN = "banerwai/micros/query/token/addr" // port: 39040
+
+	ETCD_KEY_MICROS_QUERY_AUTH   = "banerwai/micros/query/auth/addr"   // port: 39020
+	ETCD_KEY_MICROS_QUERY_USER   = "banerwai/micros/query/user/addr"   // port: 39060
+	ETCD_KEY_MICROS_QUERY_RESUME = "banerwai/micros/query/resume/addr" // port: 39070
+
+	ETCD_KEY_MICROS_QUERY_PROFILE     = "banerwai/micros/query/profile/addr"     // port: 39050
+	ETCD_KEY_MICROS_QUERY_WORKHISTORY = "banerwai/micros/query/workhistory/addr" // port: 39080
 )
 
 // command/query
 const (
-	ETCD_KEY_MICROS_COMMAND_TOKEN   = "banerwai/micros/command/token/addr"
-	ETCD_KEY_MICROS_COMMAND_PROFILE = "banerwai/micros/command/profile/addr"
-	ETCD_KEY_MICROS_COMMAND_USER    = "banerwai/micros/command/user/addr"
+	ETCD_KEY_MICROS_COMMAND_CATEGORY = "banerwai/micros/command/category/addr" // port: 36010
+	ETCD_KEY_MICROS_COMMAND_RENDER   = "banerwai/micros/command/render/addr"   // port: 36030
+
+	ETCD_KEY_MICROS_COMMAND_TOKEN = "banerwai/micros/command/token/addr" // port: 36040
+
+	ETCD_KEY_MICROS_COMMAND_AUTH   = "banerwai/micros/command/auth/addr"   // port: 36020
+	ETCD_KEY_MICROS_COMMAND_USER   = "banerwai/micros/command/user/addr"   // port: 36060
+	ETCD_KEY_MICROS_COMMAND_RESUME = "banerwai/micros/command/resume/addr" // port: 36070
+
+	ETCD_KEY_MICROS_COMMAND_PROFILE     = "banerwai/micros/command/profile/addr"     // port: 36050
+	ETCD_KEY_MICROS_COMMAND_WORKHISTORY = "banerwai/micros/command/workhistory/addr" // port: 36080
 )
