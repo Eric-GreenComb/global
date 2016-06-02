7ca9d8e6dbc209d33fec0f5d7ebfda954880b4e5
eric-greencomb <1121453646@qq.com>
2016-06-02 23:07:29 +0800

    change user dto

--- bean/user.go
+++ bean/user.go
@@ -6,19 +6,16 @@ import (
 )
 
 type User struct {
-	Id           bson.ObjectId `bson:"_id,omitempty"`          // 必须要设置bson:"_id" 不然mgo不会认为是主键
-	Invited      bson.ObjectId `bson:"invited" json:"invited"` // invited by someone
-	Email        string        `bson:"email" json:"email"`     // 全是小写
-	Pwd          string        `bson:"pwd" json:"-"`
-	Actived      bool          `bson:"actived" json:"actived"`
-	CreatedTime  time.Time     `bson:"createdtime" json:"createdtime"`
-	LastActivity time.Time     `bson:"lastactivity" json:"lastactivity"`
+	Id          bson.ObjectId `bson:"_id,omitempty"`          // 必须要设置bson:"_id" 不然mgo不会认为是主键
+	Invited     bson.ObjectId `bson:"invited" json:"invited"` // invited by someone
+	Email       string        `bson:"email" json:"email"`     // 全是小写
+	Pwd         string        `bson:"pwd" json:"-"`
+	Actived     bool          `bson:"actived" json:"actived"`
+	CreatedTime time.Time     `bson:"createdtime" json:"createdtime"`
 }
 
 type UserDto struct {
-	Id           bson.ObjectId `bson:"_id,omitempty"`          // 必须要设置bson:"_id" 不然mgo不会认为是主键
-	Invited      bson.ObjectId `bson:"invited" json:"invited"` // invited by someone
-	Email        string        `bson:"email" json:"email"`     // 全是小写
-	CreatedTime  time.Time     `bson:"createdtime" json:"createdtime"`
-	LastActivity time.Time     `bson:"lastactivity" json:"lastactivity"`
+	Id      bson.ObjectId `bson:"_id,omitempty"`          // 必须要设置bson:"_id" 不然mgo不会认为是主键
+	Invited bson.ObjectId `bson:"invited" json:"invited"` // invited by someone
+	Email   string        `bson:"email" json:"email"`     // 全是小写
 }
