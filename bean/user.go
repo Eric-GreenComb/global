79de1b6c48d6721c78a30ec4c8e1e19900d77edc
eric-greencomb <1121453646@qq.com>
2016-08-15 23:20:07 +0800

    add user form bind

--- bean/user.go
+++ bean/user.go
@@ -6,10 +6,10 @@ import (
 )
 
 type User struct {
-	Id          bson.ObjectId `bson:"_id,omitempty"`          // 必须要设置bson:"_id" 不然mgo不会认为是主键
-	Invited     bson.ObjectId `bson:"invited" json:"invited"` // invited by someone
-	Email       string        `bson:"email" json:"email"`     // 全是小写
-	Pwd         string        `bson:"pwd" json:"-"`
+	Id          bson.ObjectId `bson:"_id,omitempty"`                         // 必须要设置bson:"_id" 不然mgo不会认为是主键
+	Invited     bson.ObjectId `form:"invited" bson:"invited" json:"invited"` // invited by someone
+	Email       string        `form:"email" bson:"email" json:"email"`       // 全是小写
+	Pwd         string        `form:"pwd" bson:"pwd" json:"-"`
 	Actived     bool          `bson:"actived" json:"actived"`
 	CreatedTime time.Time     `bson:"createdtime" json:"createdtime"`
 }
