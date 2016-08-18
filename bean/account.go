bcfcbd252508bfbc7ccb812b48690c17d546c7e8
eric-greencomb <1121453646@qq.com>
2016-08-18 22:32:00 +0800

    add account api

--- bean/account.go
+++ bean/account.go
@@ -7,8 +7,8 @@ import (
 
 type Account struct {
 	Id     bson.ObjectId `bson:"_id,omitempty" json:"_id"`
-	UserId bson.ObjectId `bson:"user_id" json:"user_id"`
-	Email  string        `bson:"email" json:"email"`
+	UserId bson.ObjectId `form:"user_id" bson:"user_id" json:"user_id"`
+	Email  string        `form:"email" bson:"email" json:"email"`
 
 	MultiCurrency []MultiCurrencyAccount `bson:"multi_curreency" json:"multi_curreency"`
 
@@ -21,17 +21,17 @@ type MultiCurrencyAccount struct {
 }
 
 type Billing struct {
-	Id        bson.ObjectId `bson:"_id,omitempty" json:"_id"`
-	UserId    bson.ObjectId `bson:"user_id" json:"user_id"`
-	PayUserId bson.ObjectId `bson:"pay_user_id" json:"pay_user_id"`
-	ServiceId bson.ObjectId `bson:"service_id" json:"service_id"`
-	CostType  int           `bson:"cost_type" json:"cost_type"`
-	Memo      string        `bson:"memo" json:"memo"`
+	Id        bson.ObjectId `form:"id" bson:"_id,omitempty" json:"_id"`
+	UserId    bson.ObjectId `form:"user_id" bson:"user_id" json:"user_id"`
+	PayUserId bson.ObjectId `form:"pay_user_id" bson:"pay_user_id" json:"pay_user_id"`
+	ServiceId bson.ObjectId `form:"service_id" bson:"service_id" json:"service_id"`
+	CostType  int           `form:"cost_type" bson:"cost_type" json:"cost_type"`
+	Memo      string        `form:"memo" bson:"memo" json:"memo"`
 
 	Operate     int    `bson:"operate" json:"operate"` // 1 or -1 Operate
-	Currency    string `bson:"currency" json:"currency"`
-	Amount      int64  `bson:"amount" json:"amount"`
-	PayType     int    `bson:"paytype" json:"paytype"`
+	Currency    string `form:"currency" bson:"currency" json:"currency"`
+	Amount      int64  `form:"amount" bson:"amount" json:"amount"`
+	PayType     int    `form:"paytype" bson:"paytype" json:"paytype"`
 	PayFee      int64  `bson:"payfee" json:"payfee"`
 	PlatformFee int64  `bson:"platform_fee" json:"platform_fee"`
 	RealAmount  int64  `bson:"real_amount" json:"real_amount"`
